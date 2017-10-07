10b9eb0651dd611618842d57f6df11a984be87f8
Brian Smith <block8437@gmail.com>
2017-10-07 14:15:58 -0400

    Continued to improve browser support

--- src/api/subscription.js
+++ src/api/subscription.js
@@ -3,19 +3,29 @@ const IPFS = require('ipfs');
 const Room = require('ipfs-pubsub-room');
 
 // Establish an IPFS connection with pubsub enabled
-let additionalConfig = {};
+let additionalConfig = {
+    "Bootstrap": [
+        "/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+        "/ip4/104.236.151.122/tcp/4001/ipfs/QmSoLju6m7xTh3DuokvT3886QRYqxAzb1kShaanJgW36yx",
+        "/ip4/104.236.176.52/tcp/4001/ipfs/QmSoLnSGccFuZQJzRadHn95W2CrSFmZuTdDWP8HXaHca9z",
+        "/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLpPVmHKQ4XTPdz8tjDFgdeRFkpV8JgYq8JVJ69RrZm",
+        "/ip4/104.236.76.40/tcp/4001/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64",
+        "/ip4/128.199.219.111/tcp/4001/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu",
+        "/ip4/162.243.248.213/tcp/4001/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm",
+        "/ip4/178.62.158.247/tcp/4001/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd",
+        "/ip4/178.62.61.185/tcp/4001/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3"
+    ]
+};
 
-if ( typeof module == 'undefined' ) {
+if ( typeof window != 'undefined' ) {
     console.log("Adding additional configuration settings for the web...");
     
-    additionalConfig = {
-        "Addresses": {
-            "Swarm": [
-                "/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star"
-            ],
-            "API": '',
-            "Gateway": ''
-        }
+    additionalConfig["Addresses"] = {
+        "Swarm": [
+            "/dns4/star-signal.cloud.ipfs.team/wss/p2p-webrtc-star"
+        ],
+        "API": '',
+        "Gateway": ''
     }
 }
 
