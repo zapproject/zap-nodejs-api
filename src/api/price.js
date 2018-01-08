eae1a1768491479a3fa091b1c4207099df7bbbd0
Brian Smith <block8437@gmail.com>
2018-01-08 17:36:58 -0500

    Added price information script

---
+++ src/api/price.js
@@ -0,0 +1,124 @@
+const request = require('request');
+
+function getMarketData(loc, callback) {
+    request.get(loc, (err, res, body) => {
+        if ( err || !res ) {
+            callback(err || new Error("Failed to get response"));
+            return;
+        }
+
+        if ( res.statusCode !== 200 ) {
+            callback(new Error("Invalid status code: " + res.statusCode.toString()));
+            return;
+        }
+
+        try {
+            const parsed = JSON.parse(body);
+            callback(null, parsed);
+        }
+        catch ( err ) {
+            callback(err);
+        }
+    });
+}
+
+function getCryptopiaData(callback) {
+    getMarketData('https://www.cryptopia.co.nz/api/GetMarket/ZAP_BTC', (err, data) => {
+        if ( err ) {
+            callback(err);
+            return;
+        }
+
+        if ( !data.Success ) {
+            callback(new Error(data.Message || "Cryptopia: Failed to get market data"));
+            return;
+        }
+
+        callback(null, {
+            price: data.Data.LastPrice,
+            volume: data.Data.Volume
+        });
+    });
+}
+
+function getHitBTCData(callback) {
+    getMarketData('https://api.hitbtc.com/api/2/public/ticker/ZAPBTC', (err, data) => {
+        if ( err ) {
+            callback(err);
+            return;
+        }
+
+        callback(null, {
+            price: data.last,
+            volume: data.volume
+        });
+    });
+}
+
+function getZAPBTCAverage(callback) {
+    getCryptopiaData((err, cryptopia) => {
+        if ( err ) {
+            callback(err);
+            return;
+        }
+
+        getHitBTCData((err, hitbtc) => {
+            if ( err ) {
+                callback(err);
+                return;
+            }
+
+            // Get total volume
+            const total = cryptopia.volume + hitbtc.volume;
+
+            // Get individual percentages for markets
+            const cryptopia_percentage = cryptopia.volume / total;
+            const hitbtc_percentage = hitbtc.volume / total;
+
+            // Get weighted average
+            const weighted_avg = (cryptopia.price * cryptopia_percentage) +
+                                 (hitbtc.price * hitbtc_percentage);
+
+            callback(null, weighted_avg);
+        });
+    });
+}
+
+function getCoinbaseUSD(callback) {
+    getMarketData("https://api.coinbase.com/v2/prices/BTC-USD/spot", (err, data) => {
+        if ( err ) {
+            callback(err);
+            return;
+        }
+
+        callback(null, data.data.amount);
+    });
+}
+
+module.exports = {
+    price: (currency, callback) => {
+        if ( currency == "BTC" ) {
+            getZAPBTCAverage(callback);
+        }
+        else if ( currency == "USD" ) {
+            getZAPBTCAverage((err, btcdata) => {
+                if ( err ) {
+                    callback(err);
+                    return;
+                }
+
+                getCoinbaseUSD((err, usddata) => {
+                    if ( err ) {
+                        callback(err);
+                        return;
+                    }
+
+                    callback(null, btcdata * usddata);
+                });
+            });
+        }
+        else {
+            callback(new Error("Currency " + currency + " not supported."));
+        }
+    }
+};
