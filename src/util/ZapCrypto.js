541ed58d83998ff0a5afa9df7c5d85cbb15fef4d
Brian Smith <block8437@gmail.com>
2018-01-15 17:10:42 -0500

    Fixed the spacing in the ZapCrypto util class

--- src/util/ZapCrypto.js
+++ src/util/ZapCrypto.js
@@ -9,36 +9,36 @@ function sha256(item) {
 }
 
 class PublicKey {
-	constructor(public_hex, private_hex) {
-		this.key = null;
+    constructor(public_hex, private_hex) {
+        this.key = null;
 
-		if ( private_hex ) {
-			this.key = P224.keyFromPrivate(private_hex, 'hex');
-		}
-		else if ( public_hex ) {
-			this.key = P224.keyFromPublic(public_hex, 'hex');
-		}
-		else {
-			this.key = P224.genKeyPair();
-		}
-	}
-	// Get the public key as hex
-	getPublic() {
-		return this.key.getPublic().encode('hex', true);
-	}
+        if ( private_hex ) {
+            this.key = P224.keyFromPrivate(private_hex, 'hex');
+        }
+        else if ( public_hex ) {
+            this.key = P224.keyFromPublic(public_hex, 'hex');
+        }
+        else {
+            this.key = P224.genKeyPair();
+        }
+    }
+    // Get the public key as hex
+    getPublic() {
+        return this.key.getPublic().encode('hex', true);
+    }
 
-	// Get the private key as hex
-	getPrivate() {
-		return this.key.getPrivate().toString(16);
-	}
+    // Get the private key as hex
+    getPrivate() {
+        return this.key.getPrivate().toString(16);
+    }
 
-	// Do the key exchange
-	generateSecret(public_key) {
-		const shared = this.key.derive(public_key.key.getPublic());
-		return sha256(shared.toString(16));
-	}
+    // Do the key exchange
+    generateSecret(public_key) {
+        const shared = this.key.derive(public_key.key.getPublic());
+        return sha256(shared.toString(16));
+    }
 }
 
 module.exports = {
-	PublicKey: PublicKey
+    PublicKey: PublicKey
 };
