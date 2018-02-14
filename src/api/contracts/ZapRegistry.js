d725a80bbd7ba47f6b2568ef646472b0cc0689e2
Всеволод Грудина <vsevolod.g@smartym.pro>
2018-02-14 15:34:36 +0200

    added initiateProviderCurve method

--- src/api/contracts/ZapRegistry.js
+++ src/api/contracts/ZapRegistry.js
@@ -1,6 +1,14 @@
+// @flow
 require('babel-register');
 require('babel-polyfill');
 const ZapOracle = require('../ZapOracle');
+
+const curveType = {
+    "ZapCurveNone": 0,
+    "ZapCurveLinear": 1,
+    "ZapCurveExponential": 2,
+    "ZapCurveLogarithmic": 3
+};
 
 class ZapRegistry {
     constructor(eth) {
@@ -12,6 +20,16 @@ class ZapRegistry {
     initiateProvider({address, contract}) {
         this.address = address;
         this.contract = contract;
+    }
+
+    async initiateProviderCurve({ specifier, ZapCurveType, curveStart, curveMultiplier }) {
+        const curve = curveType[ZapCurveType];
+        return await this.contract.initiateProviderCurve(
+            specifier,
+            curve,
+            curveStart,
+            curveMultiplier
+        );
     }
 
     // get oracle by address
