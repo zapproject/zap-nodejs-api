4ced10aae14a663b8f896cabb6d88cd261b5f637
Dax Bondye <beckwithdylan@gmail.com>
2018-04-12 07:15:43 -0700

    rm oldapi

--- src/oldapi/initiateProvider.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(args) {
-    const ZapProvider = require('./provider')
-    const callback = require('../feeds/'+args.callback)
-
-    new ZapProvider(args.groupName ? args.groupName : null,
-        args.weiRate ? args.weiRate : null,
-        args.fileName ? args.fileName : null ,
-        args.action, callback);
-}
--- src/oldapi/subs.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = function(args){
-    const sub = require("./subscriber.js");
-    new sub("0x732a5496383DE6A55AE2Acc8829BE7eCE0833113", args, (data) => {
-        console.log("DATA:   ", data);
-    });
-};
