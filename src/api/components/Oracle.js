207af39bab2e51868c9a99b125d8da04b192882b
tomstlaurent <tomstlaurent.mail@gmail.com>
2018-05-28 13:55:44 -0400

    restored Oracle, Curve components

---
+++ src/api/components/Oracle.js
@@ -0,0 +1,39 @@
+const Curve = require('./Curve');
+
+class Oracle {
+    constructor(registry) {
+        this.registry = registry;
+
+        // Empty default
+        this.address = "";
+        this.public_key = "";
+        this.registry_keys = [];
+    }
+
+    // Get the curve information for a given endpoint from this Oracle
+    getCurve(endpoint, callback) {
+        const contract = this.registry.contract;
+
+        contract.getProviderCurve(this.address, endpoint).then((curveType, curveStart, curveMultiplier) => {
+            callback(null, new ZapCurve(
+                curveType,
+                curveStart,
+                curveMultiplier
+            ));
+        }).catch((err) => {
+            callback(err);
+        });
+    }
+
+    getZapBound(endpoint, callback) {
+        const contract = this.registry.contract;
+
+        contract.getZapBound(this.address, endpoint).then((totalBound) => {
+            callback(null, totalBound);
+        }).catch((err) => {
+            callback(err);
+        });
+    }
+}
+
+module.exports = Oracle;
